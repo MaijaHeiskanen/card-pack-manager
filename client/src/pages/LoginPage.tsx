cdd658e108c459641ac729f2f6dea3e2d7a4f994
Maija <maijaaaw@gmail.com>
2022-01-09 11:57:16 +0200

    Add google authentication block into client

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,3 +1,35 @@
+import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import { useTranslation } from 'react-i18next';
+
 export const LoginPage = (props: object) => {
+    const { t } = useTranslation();
+    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+    const handleLogin = async (googleData: any) => {
+        console.log({ googleData });
+        // const res = await fetch('/api/v1/auth/google', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ token: googleData.tokenId }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // });
+        // const data = await res.json(); // store returned user somehow
+    };
+
+    if (!clientId) {
+        return <div>'Client ID missing'</div>;
+    }
+
+    return (
+        <div>
+            <div>{t('loginInfo')}</div>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText={'Login with Google'}
+                onSuccess={handleLogin}
+                onFailure={handleLogin}
+                cookiePolicy={'single_host_origin'}
+            />
+        </div>
+    );
     return <div>Login page</div>;
 };
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,8 +1,4 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { CardPackViewContainer } from '../containers/CardPackViewController';
-import { Deck } from '../types/generated-types-d';
 
 export const SearchPage = (props: object) => {
     return <CardPackViewContainer />;
