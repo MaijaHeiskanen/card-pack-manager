76b61f4dbc21e3ea1a4742681cdf1b4ff55b95a5
Maija <maijaaaw@gmail.com>
2022-01-17 20:40:01 +0200

    Add custom styling to MenuBar used as header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,16 @@
 import { PrimeIcons } from 'primereact/api';
 import { useTranslation } from 'react-i18next';
 import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+
+// import { MenuItem } from 'primereact/menuitem';
+// import { LanguageSelector } from './LanguageSelector';
+// import { HeaderItem } from './HeaderItem';
+// import { Icon } from './Icon';
 
 export const Header = () => {
     const { t } = useTranslation();
-    const menuItems = [
+    const menuItems: MenuItem[] = [
         {
             label: t('browse'),
             icon: PrimeIcons.SEARCH,
@@ -20,13 +26,16 @@ export const Header = () => {
         },
     ];
 
-    return <Menubar model={menuItems} />; // TODO: Change color to primary blue and fix sub items too
+    const start = <div className="my-3 hidden sm:block">Placeholder name</div>;
+
+    return <Menubar model={menuItems} className="header" start={start} />; // TODO: Change color to primary blue and fix sub items too
 
     // return (
     //     <header className="flex flex-wrap px-3 bg-primary justify-content-between">
     //         <div className="my-3 hidden sm:block">Placeholder name</div>
     //         <div className="my-3 flex-grow-1 sm:hidden"></div>
     //         <div className="flex justify-content-end my-3 flex-wrap">
+    //             <MenuItem />
     //             <span className="mr-4">
     //                 <LanguageSelector />
     //             </span>
