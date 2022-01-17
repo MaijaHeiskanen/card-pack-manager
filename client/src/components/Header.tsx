184a4aaf14f3f99a7772eaa21905bb5107f69afe
Maija <maijaaaw@gmail.com>
2022-01-17 21:35:25 +0200

    Remove unused code

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,56 +1,45 @@
 import { PrimeIcons } from 'primereact/api';
 import { useTranslation } from 'react-i18next';
 import { Menubar } from 'primereact/menubar';
-import { MenuItem } from 'primereact/menuitem';
-
-// import { MenuItem } from 'primereact/menuitem';
-// import { LanguageSelector } from './LanguageSelector';
-// import { HeaderItem } from './HeaderItem';
-// import { Icon } from './Icon';
+import { MenuItem, MenuItemCommandParams } from 'primereact/menuitem';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const navigate = useNavigate();
+
+    const onLanguageChange = (e: MenuItemCommandParams) => {
+        i18n.changeLanguage(e.item.value);
+    };
+
+    const navigateTo = (e: MenuItemCommandParams) => {
+        navigate(e.item.to);
+    };
+
     const menuItems: MenuItem[] = [
         {
             label: t('browse'),
             icon: PrimeIcons.SEARCH,
+            command: navigateTo,
+            to: '/',
         },
         {
             label: t('language'),
             icon: PrimeIcons.FLAG,
-            items: [{ label: 'Suomi' }, { label: 'English' }],
+            items: [
+                { label: 'Suomi', value: 'fi', command: onLanguageChange },
+                { label: 'English', value: 'en', command: onLanguageChange },
+            ],
         },
         {
             label: t('login'),
             icon: PrimeIcons.USER,
+            command: navigateTo,
+            to: '/login',
         },
     ];
 
     const start = <div className="my-3 hidden sm:block">Placeholder name</div>;
 
-    return <Menubar model={menuItems} className="header" start={start} />; // TODO: Change color to primary blue and fix sub items too
-
-    // return (
-    //     <header className="flex flex-wrap px-3 bg-primary justify-content-between">
-    //         <div className="my-3 hidden sm:block">Placeholder name</div>
-    //         <div className="my-3 flex-grow-1 sm:hidden"></div>
-    //         <div className="flex justify-content-end my-3 flex-wrap">
-    //             <MenuItem />
-    //             <span className="mr-4">
-    //                 <LanguageSelector />
-    //             </span>
-    //             <HeaderItem
-    //                 icon={<Icon name={PrimeIcons.SEARCH} className={'mr-2'} />}
-    //                 linkTo={'/'}
-    //                 linkText={t('browse')}
-    //             />
-    //             <HeaderItem
-    //                 className={'ml-4'}
-    //                 icon={<Icon name={PrimeIcons.USER} className={'mr-2'} />}
-    //                 linkTo={'/login'}
-    //                 linkText={t('login')}
-    //             />
-    //         </div>
-    //     </header>
-    // );
+    return <Menubar model={menuItems} className="header" start={start} />;
 };
